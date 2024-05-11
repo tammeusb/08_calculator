ed2c7f996dc409b62750890064ac83a8c52eda04
Brock <tammeusb@gmail.com>
2024-05-11 19:16:02 -0400

    Add alternate iterative to factorial()

--- calculator.js
+++ calculator.js
@@ -17,7 +17,7 @@ const multiply = function(numbers) {
 const power = function(x,y) {
 	return x ** y
 };
-
+/*
 const factorial = function(number) {
     if (number === 0) {
       return number = 1;
@@ -25,7 +25,16 @@ const factorial = function(number) {
     else {
       return number * factorial(number - 1)
     }
-}
+}*/
+
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+};
 
 // Do not edit below this line
 module.exports = {
