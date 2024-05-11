147498526b649f5ee45e4239488298cebb28b497
Brock <tammeusb@gmail.com>
2024-05-11 16:48:23 -0400

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,34 @@
+const add = function(x,y) {
+	return x + y
+};
+
+const subtract = function(x,y) {
+	return x - y
+};
+
+const sum = function(...numbers) {
+	const array = [...numbers];
+  return array
+};
+
+const multiply = function() {
+
+};
+
+const power = function() {
+	
+};
+
+const factorial = function() {
+	
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
