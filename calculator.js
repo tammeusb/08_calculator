c9b3b3451c67cb8de5712a73ac72faf372d7d6ca
Brock <tammeusb@gmail.com>
2024-05-11 19:13:07 -0400

    Solve factorial tests via recursive function

--- calculator.js
+++ calculator.js
@@ -14,13 +14,18 @@ const multiply = function(numbers) {
   return numbers.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+    if (number === 0) {
+      return number = 1;
+    }
+    else {
+      return number * factorial(number - 1)
+    }
+}
 
 // Do not edit below this line
 module.exports = {
